3ab92037162cbdace7d33bb74a3dc220727fe6de
yy <30777685+yy-personal@users.noreply.github.com>
2023-03-29 20:33:46 +0800

    Update speedometer.js

--- js/speedometer.js
+++ js/speedometer.js
@@ -4,12 +4,12 @@ function display(color, numHours){
     console.log(color)
     if (color == 'green'){
         document.getElementById("speedometerText").innerText = "You're Good!"
-        document.getElementById("totalHours").innerText = `You have <span style='color: green;'>${numHours}</span> hours of tasks for the next 7 days!`       
+        document.getElementById("totalHours").innerText = `You have ${numHours} hours of tasks for the next 7 days!`       
     }
     else if (color == "red"){
         var extra_hrs = numHours - 44
         document.getElementById("speedometerText").innerText = "You're Stressed!"
-        document.getElementById("totalHours").innerHTML = `You have <span style='color: red;'>${numHours}</span> hours of tasks for the next 7 days! <br>Which is <span style='color: red;'>${extra_hrs}</span> more than the recommended <span style='color: green;'>44</span> working hours in Singapore!`
+        document.getElementById("totalHours").innerHTML = `You have ${numHours} hours of tasks for the next 7 days! <br>Which is <span style='color: red;'>${extra_hrs}</span> more than the recommended <span style='color: green;'>44</span> working hours in Singapore!`
 
     }
     return
